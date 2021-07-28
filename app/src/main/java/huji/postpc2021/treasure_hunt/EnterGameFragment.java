cc91783c40824287a83d057dabe8f8986a4f10fc
Ido Sagiv <ido10796@gmail.com>
2021-07-28 17:21:47 +0300

    create layouts fragments

---
+++ app/src/main/java/huji/postpc2021/treasure_hunt/EnterGameFragment.java
@@ -0,0 +1,29 @@
+package huji.postpc2021.treasure_hunt;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EnterGameFragment extends Fragment {
+    public EnterGameFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_enter_game, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
--- app/src/main/java/huji/postpc2021/treasure_hunt/MainActivity.java
+++ app/src/main/java/huji/postpc2021/treasure_hunt/MainActivity.java
@@ -1,8 +1,14 @@
 package huji.postpc2021.treasure_hunt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
     }
 }
