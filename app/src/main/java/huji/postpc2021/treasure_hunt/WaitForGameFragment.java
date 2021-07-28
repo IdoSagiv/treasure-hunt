b6aafa8f3f2d5141d9cce9dcda3125d6a3131525
Ido Sagiv <ido10796@gmail.com>
2021-07-28 20:58:28 +0300

    ask before leaving game

--- app/src/main/java/huji/postpc2021/treasure_hunt/WaitForGameFragment.java
+++ app/src/main/java/huji/postpc2021/treasure_hunt/WaitForGameFragment.java
@@ -1,9 +1,12 @@
 package huji.postpc2021.treasure_hunt;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -12,6 +15,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class WaitForGameFragment extends Fragment {
+    private View view;
+
     public WaitForGameFragment() {
         // Required empty public constructor
     }
@@ -20,10 +25,9 @@ public class WaitForGameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_wait_for_game, container, false);
+        view = inflater.inflate(R.layout.fragment_wait_for_game, container, false);
 
-        view.findViewById(R.id.buttonLeaveGameInWaitScreen).setOnClickListener(v ->
-                Navigation.findNavController(view).navigate(R.id.action_waitForGameFragment_to_homeScreenFragment));
+        view.findViewById(R.id.buttonLeaveGameInWaitScreen).setOnClickListener(this::leaveGame);
 
         return view;
     }
@@ -31,5 +35,32 @@ public class WaitForGameFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // handle BackPressed
+        requireActivity().getOnBackPressedDispatcher().addCallback(new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                leaveGame(view);
+            }
+        });
+    }
+
+    private void leaveGame(View view) {
+        DialogInterface.OnClickListener dialogClickListener = (dialog, which) -> {
+            switch (which) {
+                case DialogInterface.BUTTON_POSITIVE: {
+                    Navigation.findNavController(view).navigate(R.id.action_waitForGameFragment_to_homeScreenFragment);
+                    break;
+                }
+                case DialogInterface.BUTTON_NEGATIVE:
+                    break;
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this.getContext());
+        builder.setMessage("leave game?")
+                .setPositiveButton("Yes", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener)
+                .show();
     }
 }
