199edf4a33f0cbfacbdb1a50041411b9749d96ba
Ido Sagiv <ido10796@gmail.com>
2021-08-04 17:34:26 +0300

    add osm dependency and initial map fragment

---
+++ app/src/main/java/huji/postpc2021/treasure_hunt/PlayerGameFragment.java
@@ -0,0 +1,31 @@
+package huji.postpc2021.treasure_hunt;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+public class PlayerGameFragment extends Fragment {
+    public PlayerGameFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_player_game, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
