4da39b3ee23512c4a2e8e1bce8263a74564d8b04
Ido Sagiv <ido10796@gmail.com>
2021-08-17 10:59:39 +0300

    add location to the clue

--- app/src/main/java/huji/postpc2021/treasure_hunt/DataObjects/Clue.java
+++ app/src/main/java/huji/postpc2021/treasure_hunt/DataObjects/Clue.java
@@ -1,5 +1,7 @@
 package huji.postpc2021.treasure_hunt.DataObjects;
 
+import com.google.firebase.firestore.GeoPoint;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -7,15 +9,15 @@ import java.util.UUID;
 public class Clue {
     private final String id;
     private String description;
-    //    private GeoPoint location;
+    private GeoPoint location;
     private List<Player> visitedPlayers;
     private int index;
     // TODO AR
 
-    public Clue(String description, int index /*,GeoPoint location*/) {
+    public Clue(String description, int index ,GeoPoint location) {
         this.id = UUID.randomUUID().toString();
         this.description = description;
-//        this.location = location;
+        this.location = location;
         this.visitedPlayers = new ArrayList<>();
         this.index = index;
     }
@@ -26,5 +28,13 @@ public class Clue {
 
     public String getId() {
         return id;
+    }
+
+    public GeoPoint getLocation() {
+        return location;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
